4815c98edfdde5425b0e0b0fe0ff13f8be89341b
ferculus <165941104+Ferhsx@users.noreply.github.com>
2025-04-09 12:51:54 -0300

    Mais um pouco

--- src/script.js
+++ src/script.js
@@ -1,2 +1,30 @@
 //inicio codigo
 let pontosDisponiveis = 3;
+
+const pontosDisplay = document.getElementById("pontos");
+const nivelDisplay = document.getElementById("nivel");
+
+const personagens = [
+    {
+      nome: "Kael Ignis",
+      cor: "#FF4500",
+      ramos: {
+        Combate: ["Golpe Flamejante", "Explosão Ígnea", "Lança de Fogo"],
+        Investigação: ["Análise de Cinzas", "Visão Térmica", "Rastreamento por Calor"],
+        Poder: ["Domínio do Fogo", "Fênix Interior", "Erupção Total"]
+      }
+    },
+    {
+      nome: "Lysa Glacius",
+      cor: "#00BFFF",
+      ramos: {
+        Combate: ["Lâmina de Gelo", "Nevasca Cortante", "Encerramento Ártico"],
+        Investigação: ["Sentir Vibrações", "Escanear Ambiente", "Mapa de Temperatura"],
+        Poder: ["Congelamento Global", "Espinhos de Gelo", "Prisão Congelante"]
+      }
+    }
+  ];
+
+  const conteiner = document.getElementById("arvConteiner");
+
+  
